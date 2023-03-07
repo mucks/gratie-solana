3d2c6fa1ee32b31687c7af03c1245ea81a9d2a24
mucks <mail@mucks.dev>
2023-03-07 16:01:46 +0700

    updated contract to newer anchor version

--- programs/gratie-solana/src/lib.rs
+++ programs/gratie-solana/src/lib.rs
@@ -1,15 +1,224 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_spl::token;
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v3};
+use mpl_token_metadata::state::Metadata;
+use mpl_token_metadata::state::TokenMetadataAccount;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// The Id of our program.
+declare_id!("6Ki5zkofcL2h3q1F1T9WYUPWwbu9DogCGB5vmLGyi1Ce");
 
+// Why is rust showing an error here but compiling successfully?
 #[program]
 pub mod gratie_solana {
+
+    use mpl_token_metadata::state::Collection;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn get_metadata(ctx: Context<GetMetadata>) -> Result<Pubkey> {
+        let (metadata, _) = Pubkey::find_program_address(
+            &[
+                mpl_token_metadata::state::PREFIX.as_bytes(),
+                mpl_token_metadata::id().as_ref(),
+                ctx.accounts.mint_authority.key.as_ref(),
+            ],
+            &mpl_token_metadata::id(),
+        );
+        Ok(metadata)
+    }
+
+    // Input wallet address
+    pub fn get_company_license(ctx: Context<GetCompanyLicense>) -> Result<Option<Collection>> {
+        msg!("Getting Company License");
+
+        let mint_metadata = Metadata::from_account_info(&ctx.accounts.metadata.to_account_info())?;
+        msg!("Mint Metadata Assigned");
+
+        Ok(mint_metadata.collection)
+    }
+
+    /*
+        Will be minted by a company when they want to use our service.
+        This will be a unique token for each company.
+        One wallet address can only have one company license.
+    */
+
+    // TODO: implement a check to see if the wallet address already has a company license.
+    pub fn mint_company_license(
+        ctx: Context<MintCompanyLicense>,
+        creator_key: Pubkey,
+        title: String,
+        url: String,
+    ) -> Result<()> {
+        msg!("Minting Company License");
+
+        // Create a new account for the token.
+        // CPI stands for cross program invocation.
+        let cpi_accounts = token::MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+
+        msg!("CPI Accounts Assigned");
+
+        // Get the CPI Program
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        msg!("CPI Program Assigned");
+
+        // Create a new CPI Context
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("CPI Context Assigned");
+
+        // Mint token to the token account.
+        token::mint_to(cpi_ctx, 1)?;
+        msg!("Token Minted !!!");
+
+        // Create the metadata accounts.
+
+        // Get the account info
+        let account_info = vec![
+            // Metadata account
+            ctx.accounts.metadata.to_account_info(),
+            // Mint
+            ctx.accounts.mint.to_account_info(),
+            // Mint Authority, the company that is minting the license.
+            ctx.accounts.mint_authority.to_account_info(),
+            // Payer, the company that is minting the license.
+            ctx.accounts.payer.to_account_info(),
+            // The program that is used to create the metadata accounts.
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Account Info Assigned");
+
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+
+        let symbol: String = "GRATIE".to_string();
+
+        invoke(
+            &create_metadata_accounts_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                symbol,
+                url,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        msg!("Metadata Accounts Created !!!");
+
+        let master_edition_infos = vec![
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.master_edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+
+        msg!("Master Edition Nft Minted !!!");
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct GetMetadata<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct GetCompanyLicense<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct MintCompanyLicense<'info> {
+    // ? The account of the company that is minting the license.
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    // ? The account of the company that is minting the license.
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub rent: AccountInfo<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+
+    // Related to metaplex
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+
+    // Related to metaplex
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+
+    // The token program is the program that will be used to mint the token.
+    pub token_program: Program<'info, token::Token>,
+    // Create new accounts, allocate account data, assign accounts to owning programs, transfer lamports from System Program owned accounts and pay transaction fees.
+    pub system_program: Program<'info, System>,
+
+    // ? The program that is used to create the metadata accounts.
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
+}
--- tests/gratie-solana.ts
+++ tests/gratie-solana.ts
@@ -1,16 +1,148 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, Wallet, AnchorProvider, SystemProgram } from "@project-serum/anchor";
 import { GratieSolana } from "../target/types/gratie_solana";
+import { TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction, getAssociatedTokenAddress, createInitializeMintInstruction, MINT_SIZE } from '@solana/spl-token'
+
 
 describe("gratie-solana", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.GratieSolana as Program<GratieSolana>;
+  const program = anchor.workspace.GratieSolana as Program<GratieSolana>
+  const wallet = anchor.AnchorProvider.env().wallet as Wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('get-metadata', async () => {
+    await testGetMetadata(program, wallet);
   });
+
+  // it("mint-company-license", async () => {
+  //   await testMintCompanyLicense(program, wallet);
+  // });
 });
+
+const testMintCompanyLicense = async (program: Program<GratieSolana>, wallet: Wallet) => {
+  const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+
+  const lamports: number =
+    await program.provider.connection.getMinimumBalanceForRentExemption(
+      MINT_SIZE
+    );
+
+  const getMetadata = async (
+    mint: anchor.web3.PublicKey
+  ): Promise<anchor.web3.PublicKey> => {
+    return (
+      await anchor.web3.PublicKey.findProgramAddress(
+        [
+          Buffer.from("metadata"),
+          TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+          mint.toBuffer(),
+        ],
+        TOKEN_METADATA_PROGRAM_ID
+      )
+    )[0];
+  };
+
+  const getMasterEdition = async (
+    mint: anchor.web3.PublicKey
+  ): Promise<anchor.web3.PublicKey> => {
+    return (
+      await anchor.web3.PublicKey.findProgramAddress(
+        [
+          Buffer.from("metadata"),
+          TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+          mint.toBuffer(),
+          Buffer.from("edition"),
+        ],
+        TOKEN_METADATA_PROGRAM_ID
+      )
+    )[0];
+  };
+
+  const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+  const NftTokenAccount = await getAssociatedTokenAddress(
+    mintKey.publicKey,
+    wallet.publicKey
+  );
+  console.log("NFT Account: ", NftTokenAccount.toBase58());
+
+
+  const mint_tx = new anchor.web3.Transaction().add(
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: wallet.publicKey,
+      newAccountPubkey: mintKey.publicKey,
+      space: MINT_SIZE,
+      programId: TOKEN_PROGRAM_ID,
+      lamports,
+    }),
+    createInitializeMintInstruction(
+      mintKey.publicKey,
+      0,
+      wallet.publicKey,
+      wallet.publicKey
+    ),
+    createAssociatedTokenAccountInstruction(
+      wallet.publicKey,
+      NftTokenAccount,
+      wallet.publicKey,
+      mintKey.publicKey
+    )
+  );
+
+  const res = await program.provider.sendAndConfirm(mint_tx, [mintKey]);
+  console.log(
+    await program.provider.connection.getParsedAccountInfo(mintKey.publicKey)
+  );
+
+  console.log("Account: ", res);
+  console.log("Mint key: ", mintKey.publicKey.toString());
+  console.log("User: ", wallet.publicKey.toString());
+
+  const metadataAddress = await getMetadata(mintKey.publicKey);
+  const masterEdition = await getMasterEdition(mintKey.publicKey);
+
+  console.log("Metadata address: ", metadataAddress.toBase58());
+  console.log("MasterEdition: ", masterEdition.toBase58());
+
+  const tx = await program.methods.mintCompanyLicense(
+    mintKey.publicKey,
+    "",
+    "Sample Company License",
+  )
+    .accounts({
+      mintAuthority: wallet.publicKey,
+      mint: mintKey.publicKey,
+      tokenAccount: NftTokenAccount,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      metadata: metadataAddress,
+      tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+      payer: wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      masterEdition: masterEdition,
+    },
+    )
+    .rpc();
+
+  console.log("Your transaction signature", tx);
+};
+
+
+const testGetMetadata = async (program: Program<GratieSolana>, wallet: Wallet) => {
+  const meta = await program.methods.getMetadata().accounts({
+    mintAuthority: wallet.publicKey,
+  }).rpc();
+  console.log(meta);
+
+  const keypair = anchor.web3.Keypair.generate();
+
+
+
+  const collection = await program.methods.getCompanyLicense().accounts({
+    mintAuthority: wallet.publicKey,
+    metadata: keypair.publicKey,
+  }).rpc();
+
+  console.log(collection);
+}
