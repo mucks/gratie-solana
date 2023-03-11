632a4be80d38e01d643d6e8531dbf7d841d8bb11
mucks <mail@mucks.dev>
2023-03-11 17:45:10 +0700

    modified script

--- build_and_copy_to_frontend.sh
+++ build_and_copy_to_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-anchor build && anchor deploy
+anchor build && anchor deploy && anchor run test
 
 yes | cp -r target/idl ../gratie-frontend/src/lib/
 yes | cp -r target/types ../gratie-frontend/src/lib/
